45dd669f64d1191d4ebe941dc5a54f3a0870f18e
Stephen von Takach <steve@vontaka.ch>
2020-07-21 15:00:59 +1000

    spec: ensure it works with union types

--- spec/core_ext/presence_spec.cr
+++ spec/core_ext/presence_spec.cr
@@ -18,4 +18,21 @@ describe "core_ext/presence" do
     "   \t\n  \n".present?.should eq nil
     "  data ".present?.should eq "  data "
   end
+
+  it "works with union types" do
+    me = 3
+    object = case me
+             when 1
+               "  some stry "
+             when 2
+               [1, 2, 3, 4]
+             when 3
+               {1, 2, 3}
+             when 4
+               {"me" => 123}
+             end
+
+    typeof(object).to_s.should eq("(Array(Int32) | Hash(String, Int32) | String | Tuple(Int32, Int32, Int32) | Nil)")
+    (!!object.present?).should eq(true)
+  end
 end
