60ca6d072e113029b6a7b44be029cf12d2924bb1
Kim Burgess <kim@place.technology>
2020-07-23 20:37:26 +1000

    feat(args): support double splat arg

--- spec/macro/args_spec.cr
+++ spec/macro/args_spec.cr
@@ -5,10 +5,19 @@ def args_test(a, b, c)
   args
 end
 
+def args_test2(d, **e)
+  args
+end
+
 describe "macro/args" do
   it "creates a NamedTuple from surround args" do
     result = args_test "foo", "bar", "baz"
     result.should eq({a: "foo", b: "bar", c: "baz"})
+  end
+
+  it "includes the double splat arg if it exists" do
+    result = args_test2 "test", hello: "world"
+    result.should eq({d: "test", e: {hello: "world"}})
   end
 end
 
